a3e3d5bc9c50b2fd17c035997a0d6fa286b8118b
joao <joaovitorcruzstragevitch@gmail.com>
2026-05-11 17:48:07 -0300

    String Conexão

---
+++ Lojinha/DAL/ClientesDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lojinha.Modelos;
+
+namespace Lojinha.DAL
+{
+    public class ClientesDAL
+    {
+
+    }
+}
---
+++ Lojinha/DAL/Dados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lojinha.DAL
+{
+    public class Dados
+    {
+        public static string StringConexao
+        {
+            get
+            {
+                return "server = Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dblojinha;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+            }
+                
+      
+
+
+        }
+            
+           
+            
+        
+            
+         
+    }
+
+}
