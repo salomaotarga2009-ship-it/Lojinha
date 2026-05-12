50233a7c91fd9f9274f983bbcf39f011fa23c72a
joao <joaovitorcruzstragevitch@gmail.com>
2026-05-12 16:06:10 -0300

    Adicionei Alterar e Excluir

--- Lojinha/DAL/ClientesDAL.cs
+++ Lojinha/DAL/ClientesDAL.cs
@@ -55,7 +55,119 @@ namespace Lojinha.DAL
             {
                 cn.Close();
             }
-                
+
+        }
+
+        public void Alterar(ClienteInformation cliente)
+        {
+            //Conexão com o banco de dados 
+            SqlConnection cn = new SqlConnection(Dados.StringConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "altera.cliente";
+                //Parametros Da Stored Procedure
+                SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+                pcodigo.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pcodigo);
+
+                SqlParameter pnome = new SqlParameter("@nome", SqlDbType.VarChar, 100);
+                pnome.Value = cliente.Nome;
+                cmd.Parameters.Add(pnome);
+
+                SqlParameter pemail = new SqlParameter("@email", SqlDbType.VarChar, 100);
+                pemail.Value = cliente.Email;
+                cmd.Parameters.Add(pnome);
+
+                SqlParameter ptelefone = new SqlParameter("@telefone", SqlDbType.VarChar, 100);
+                ptelefone.Value = cliente.Telefone;
+                cmd.Parameters.Add(pnome);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                cliente.Codigo = (Int32)cmd.Parameters["@codigo"].Value;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro ao acessa banco de dados. " + ex.Number);
+            }
+            catch
+            {
+                throw new Exception("Erro desconhecido ao acessar banco de dados. ");
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+        public void Excluir(int codigo)
+        {
+            //Conexão com o banco de dados 
+            SqlConnection cn = new SqlConnection(Dados.StringConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "exclui.cliente";
+                //Parametros Da Stored Procedure
+                SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+                pcodigo.Value = codigo;
+                cmd.Parameters.Add(pcodigo);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro ao acessa banco de dados. " + ex.Number);
+            }
+            catch
+            {
+                throw new Exception("Erro desconhecido ao acessar banco de dados. ");
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+        public DataTable Listagem(string filtro)
+        {
+            //Conexão com o banco de dados 
+            SqlConnection cn = new SqlConnection(Dados.StringConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "lista.cliente";
+                //Parametros Da Stored Procedure
+                SqlParameter pfiltro = new SqlParameter("@filtro", SqlDbType.VarChar);
+                pfiltro.Value = filtro;
+                DataTable tabela = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabela);
+                return tabela;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro ao acessa banco de dados. " + ex.Number);
+            }
+            catch
+            {
+                throw new Exception("Erro desconhecido ao acessar banco de dados. ");
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+
+
         }
     }
 }
